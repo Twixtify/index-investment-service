316f63228f0b8eed71cb7ef8f96fb926d6fbd586
Twixtify <snakelover92@hotmail.com>
2024-12-19 23:54:10 +0100

    feat #1: added scramble and step mutation, also moved definitions in order of execution

--- investopy/algorithms/genetic/definitions.py
+++ investopy/algorithms/genetic/definitions.py
@@ -6,28 +6,6 @@ T = TypeVar("T", bound=int | float)
 S = TypeVar("S", bound=int | float | str)
 
 
-class Mutation(ABC):
-    """
-    Define a mutation protocol.
-
-    Mutation is the process of changing a gene of an individual by random chance.
-    This process is used to maintain and introduce diversity in the population.
-    """
-
-    @abstractmethod
-    def mutate(self, *args) -> None:
-        """
-        Call upon this method to potentially mutate individuals in the population.
-
-        Here is a list of possible mutation strategies:
-        1) Perturbation: Change at random some gene of the individual by perturbing its value.
-        2) Swap: Choose two genes and swap their position.
-        3) Scramble: Choose a random length segment and interchange genes in this segment.
-        4) Inversion: Choose a random length segment and reverse the order of genes in it.
-        """
-        raise NotImplementedError()
-
-
 class Gene(ABC):
     """
     Define a gene for a chromosome.
@@ -59,27 +37,6 @@ class Gene(ABC):
         raise NotImplementedError()
 
 
-class Terminate(ABC):
-    """
-    Define the termination protocol of the population.
-    """
-
-    @abstractmethod
-    def termination(self, *args, **kwargs) -> bool:
-        """
-        Determine if the population should stop evolving.
-
-        Some stopping conditions could be:
-        1) Maximum number of generations reached.
-        2) Timelimit.
-        3) A fitness limit reached.
-        4) Population stagnation, the average fitness of the population does not change between generations in
-        comparison to some predefined metric.
-        5) Time stagnation, the average fitness of the population does not change within a certain amount of time.
-        """
-        raise NotImplementedError()
-
-
 class Chromosome(ABC):
     """
     Define a chromosome protocol of the population.
@@ -104,26 +61,6 @@ class Chromosome(ABC):
         The fitness is determined in relation to an objective function which is specific for a population.
         The population defines the problem we want to solve.
         Therefore, the fitness is determined by the Population and not the individual chromosome.
-        """
-        raise NotImplementedError()
-
-
-class Reproduction(ABC):
-    """
-    Define the protocol for generating children (crossover).
-
-    Crossover is the process of creating children from individuals.
-    There exists many types of crossover methods. Below some of them have been listed.
-
-    1) Uniform crossover is the process of swapping genes between two individuals to produce two children.
-    2) One point crossover is the process of picking a point in each individuals genome and swapping the
-    tail after this point between them.
-    """
-
-    @abstractmethod
-    def breed(self, parents: Sequence[Chromosome]) -> Sequence[Chromosome]:
-        """
-        Parents breed, also known as the chromosomes undergo crossover, to produce new individuals (children).
         """
         raise NotImplementedError()
 
@@ -161,6 +98,48 @@ class Recombination(ABC):
         raise NotImplementedError()
 
 
+class Reproduction(ABC):
+    """
+    Define the protocol for generating children (crossover).
+
+    Crossover is the process of creating children from individuals.
+    There exists many types of crossover methods. Below some of them have been listed.
+
+    1) Uniform crossover is the process of swapping genes between two individuals to produce two children.
+    2) One point crossover is the process of picking a point in each individuals genome and swapping the
+    tail after this point between them.
+    """
+
+    @abstractmethod
+    def breed(self, parents: Sequence[Chromosome]) -> Sequence[Chromosome]:
+        """
+        Parents breed, also known as the chromosomes undergo crossover, to produce new individuals (children).
+        """
+        raise NotImplementedError()
+
+
+class Mutation(ABC):
+    """
+    Define a mutation protocol.
+
+    Mutation is the process of changing a gene of an individual by random chance.
+    This process is used to maintain and introduce diversity in the population.
+    """
+
+    @abstractmethod
+    def mutate(self, chromosome: Chromosome) -> None:
+        """
+        Call upon this method to potentially mutate individuals in the population.
+
+        Here is a list of possible mutation strategies:
+        1) Perturbation: Change at random some gene of the individual by perturbing its value.
+        2) Swap: Choose two genes and swap their position.
+        3) Scramble: Choose a random length segment and interchange genes in this segment.
+        4) Inversion: Choose a random length segment and reverse the order of genes in it.
+        """
+        raise NotImplementedError()
+
+
 class ObjectiveFunction(ABC):
 
     @property
@@ -168,6 +147,27 @@ class ObjectiveFunction(ABC):
     def fitness(self, *args, **kwargs) -> T:
         """
         Calculate the fitness of a candidate solution or chromosome.
+        """
+        raise NotImplementedError()
+
+
+class Terminate(ABC):
+    """
+    Define the termination protocol of the population.
+    """
+
+    @abstractmethod
+    def termination(self) -> bool:
+        """
+        Determine if the population should stop evolving.
+
+        Some stopping conditions could be:
+        1) Maximum number of generations reached.
+        2) Timelimit.
+        3) A fitness limit reached.
+        4) Population stagnation, the average fitness of the population does not change between generations in
+        comparison to some predefined metric.
+        5) Time stagnation, the average fitness of the population does not change within a certain amount of time.
         """
         raise NotImplementedError()
 
--- investopy/algorithms/genetic/mutation.py
+++ investopy/algorithms/genetic/mutation.py
@@ -1,16 +1,82 @@
+import logging
 import random
 from dataclasses import dataclass
+from typing import Optional
 
-from investopy.algorithms.genetic.definitions import Mutation
+from definitions import Mutation, Chromosome
 
 
 @dataclass
 class UniformStepMutation(Mutation):
     mut_prob: float
+    min_threshold: Optional[int] = None
+    max_threshold: Optional[int] = None
 
-    def mutate(self, value: int, step: int) -> None:
+    def mutate(self, chromosome: Chromosome, step: int) -> None:
         """
-        Mutate an integer by picking a random integer in an interval [-step, step] inclusively around the value.
+        Mutate an integer by picking a random integer in an interval [-step, step] around the gene parameter value.
         """
-        if random.random() <= self.mut_prob:
-            value += random.randint(-step, step)
+        if not all(isinstance(gene.parameter, int) for gene in chromosome.genes):
+            logging.warning("Cannot perform uniform step mutation on non-integer genes.")
+            return None
+        for i, gene in enumerate(chromosome.genes):
+            if random.random() <= self.mut_prob:
+                gene.parameter += random.randint(-step, step)
+                # Set gene parameter to threshold if it exists
+                if self.min_threshold is not None and gene.parameter < self.min_threshold:
+                    gene.parameter = self.min_threshold
+                elif self.max_threshold is not None and gene.parameter > self.max_threshold:
+                    gene.parameter = self.max_threshold
+
+
+@dataclass
+class Scramble(Mutation):
+    """
+    Scramble.
+
+    The process of selecting a subset of genes and scramble their parameters.
+    The picked genes does not have to be contiguous i.e. adjacent to each other.
+
+    Args:
+        mut_prob (float): mutation probability
+    """
+    mut_prob: float
+
+    def mutate(self, chromosome: Chromosome, scramble_size: int) -> None:
+        """Mutate the chromosome"""
+        # Check if mutation should occur
+        if random.random() > self.mut_prob:
+            return None
+        # Make sure scramble size does not exceed genome
+        scramble_size = min(len(chromosome.genes), scramble_size)
+        # Randomly select indices to scramble
+        indices = random.sample(range(len(chromosome.genes)), scramble_size)
+        # Extract gene parameters corresponding to indices
+        parameters_to_scramble = [chromosome.genes[i].parameter for i in indices]
+        # Shuffle the parameters
+        random.shuffle(parameters_to_scramble)
+        # Insert shuffled parameters at indices position
+        for i, index in enumerate(indices):
+            chromosome.genes[index].parameter = parameters_to_scramble[i]
+
+
+if __name__ == "__main__":
+    from investopy.algorithms.genetic.gene import StockGene
+    from investopy.algorithms.genetic.chromosome import StockChromosome
+    from investopy.algorithms.genetic.objective_function import IndexWeight
+
+    func = IndexWeight()
+    sg1 = StockGene(name="sg1", price=1, amount=1, weight=0)
+    sg2 = StockGene(name="sg2", price=1, amount=2, weight=0)
+    sg3 = StockGene(name="sg3", price=1, amount=3, weight=0)
+    sg4 = StockGene(name="sg4", price=1, amount=4, weight=0)
+    individual1 = StockChromosome(genes=[sg1, sg2, sg3, sg4])
+    individual1.fitness = func.fitness(individual1)
+
+    uformstep = UniformStepMutation(0.5, min_threshold=0)
+    scramble = Scramble(1)
+
+    # uformstep.mutate(individual1, 10)
+    scramble.mutate(individual1, 7)
+
+    print(individual1.genes)
